f1d01bc44dc7dbf8f7d11c8eb40364cec28e1ca6
Sasa <sasastanisic4@gmail.com>
2023-06-15 17:21:01 +0200

    Started working on adding new products

--- CS323-PZ01/Main.c
+++ CS323-PZ01/Main.c
@@ -1,9 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define PRODUCT_NUM 2
+
+typedef struct {
+
+	char name[30];
+	char code[50];
+	float price;
+	int quantity;
+
+} Product;
+
+void enter_data(Product* product) {
+	printf("Name -> ");
+	gets(product->name);
+	printf("Code -> ");
+	gets(product->code);
+	printf("Price -> ");
+	scanf_s("%f", &product->price);
+	printf("Quantity -> ");
+	scanf_s("%d", &product->quantity);
+}
+
+void welcome_menu() {
+	printf("Welcome to shop!\n");
+}
 
 int main() {
 
-	printf("Initial commit!");
+	welcome_menu();
+
+	Product product[PRODUCT_NUM];
+
+	printf("Enter data for %d products:\n", PRODUCT_NUM);
+	for (int i = 0; i < PRODUCT_NUM; i++) {
+		printf("Product %d:\n", i + 1);
+		enter_data(&product[i]);
+		while (getchar() != '\n');
+	}
 
 	return 0;
 }
